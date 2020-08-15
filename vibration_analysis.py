35c4902b8c14587f100905ee9bca4a07d1040d48
Tim Anderegg <timothy.anderegg@gmail.com>
2020-08-15 18:11:46 -0400

    Fixed FFT code.

--- vibration_analysis.py
+++ vibration_analysis.py
@@ -1,70 +1,71 @@
 # SOURCE: https://blog.endaq.com/matlab-vs-python-speed-for-vibration-analysis-free-download
 
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.fftpack import fft
-import tkinter as tk
-from tkinter import filedialog
 import time
 
-#Prompt user for file
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename(filetypes=[("Two Column CSV","*.csv")])
-print(file_path)
- 
-#Load Data (assumes two column array
-tic = time.clock()
-t, x = np.genfromtxt(file_path,delimiter=',', unpack=True)
-toc = time.clock()
-print("Load Time:",toc-tic)
- 
-#Determine variables
-N = np.int(np.prod(t.shape))#length of the array
-Fs = 1/(t[1]-t[0])  #sample rate (Hz)
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from scipy.fftpack import fft
+from datetime import datetime
+
+def convert_date(date):
+    return float(datetime.strptime(date.decode(), '%Y-%m-%d %H:%M:%S.%f').timestamp())
+
+file_path = 'vibration_data+2020-08-15_153757.csv'
+dataset = file_path.split('.')[0]
+
+# Load Data (assumes two column array
+ts, ax, ay, az = np.genfromtxt(file_path,
+                               delimiter=',',
+                               skip_header=1,
+                               unpack=True,
+                               converters={0: convert_date})
+
+# Determine variables
+N = np.int(np.prod(ts.shape)) # length of the array
+Fs = 1/(ts[1]-ts[0])  # sample rate (Hz)
+print(f'Fs:{Fs}')
 T = 1/Fs;
-print("# Samples:",N)
- 
-#Plot Data
-tic = time.clock()
+print(f'T:{T}')
+print("# Samples: ", N)
+
+# Plot Data
 plt.figure(1)  
-plt.plot(t, x)
+plt.plot(ts, az) # Plot Z g value for now
 plt.xlabel('Time (seconds)')
-plt.ylabel('Accel (g)')
+plt.ylabel('Z Accel (g)')
 plt.title(file_path)
 plt.grid()
-toc = time.clock()
-print("Plot Time:",toc-tic)
- 
-#Compute RMS and Plot
-tic = time.clock()
-w = np.int(np.floor(Fs)); #width of the window for computing RMS
-steps = np.int_(np.floor(N/w)); #Number of steps for RMS
-t_RMS = np.zeros((steps,1)); #Create array for RMS time values
-x_RMS = np.zeros((steps,1)); #Create array for RMS values
+plt.savefig(f'{dataset}.png')
+
+# Compute RMS and Plot
+w = np.int(np.floor(Fs)); # width of the window for computing RMS
+steps = np.int_(np.floor(N/w)); # Number of steps for RMS
+ts_RMS = np.zeros((steps,1)); # Create array for RMS time values
+az_RMS = np.zeros((steps,1)); # Create array for RMS values
+
 for i in range (0, steps):
-    t_RMS[i] = np.mean(t[(i*w):((i+1)*w)]);
-    x_RMS[i] = np.sqrt(np.mean(x[(i*w):((i+1)*w)]**2));  
+    ts_RMS[i] = np.mean(ts[(i*w):((i+1)*w)])
+    az_RMS[i] = np.sqrt(np.mean(az[(i*w):((i+1)*w)]**2))
+
 plt.figure(2)  
-plt.plot(t_RMS, x_RMS)
+plt.plot(ts_RMS, az_RMS)
 plt.xlabel('Time (seconds)')
-plt.ylabel('RMS Accel (g)')
+plt.ylabel('RMS Z Accel (g)')
 plt.title('RMS - ' + file_path)
 plt.grid()
-toc = time.clock()
-print("RMS Time:",toc-tic)
- 
-#Compute and Plot FFT
-tic = time.clock()
+plt.savefig(f'{dataset}-RMS.png')
+
+# Compute and Plot FFT
 plt.figure(3)  
-xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-yf = fft(x)
+xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+yf = fft(az)
 plt.plot(xf, 2.0/N * np.abs(yf[0:np.int(N/2)]))
 plt.grid()
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Accel (g)')
+plt.ylabel('Z Accel (g)')
 plt.title('FFT - ' + file_path)
-toc = time.clock()
-print("FFT Time:",toc-tic)
 plt.show()
+plt.savefig(f'{dataset}-FFT.png')
  
