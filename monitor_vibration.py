76348836211e218a203f3cfacb211cfdedf50736
Tim Anderegg <timothy.anderegg@gmail.com>
2020-08-16 11:41:56 -0400

    Attempt proper units.

--- monitor_vibration.py
+++ monitor_vibration.py
@@ -1,4 +1,5 @@
 import csv
+import time
 
 from datetime import datetime
 from mpu6050_i2c import *
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     filename = 'vibration_data_{}.csv'.format(datetime.strftime(timestamp, '%Y-%m-%d_%H%M%S'))
 
     sampling_rate = 50
-    frame_length = 1.0 / sampling_rate
+    frame_length = time.delta(seconds=1.0) / sampling_rate
 
     with open(filename, 'w') as outfile:
         outfile_writer = csv.writer(outfile,
@@ -22,20 +23,19 @@ if __name__ == '__main__':
         start_time = datetime.now()
         last_time = start_time
         while 1:
-            current_time = datetime.now()
+            sample_time = datetime.now()
+            total_elapsed_time = sample_time - start_time
             try:
                 ax,ay,az,wx,wy,wz,temp = mpu6050_conv() # read and convert mpu6050 data
             except Exception as e:
                 print('Exception encountered, ignoring: {}'.format(e))
                 continue
-            
-            total_elapsed_time = current_time - start_time
 
             print('accel [g]: x = {}, y = {}, z = {}'.format(ax,ay,az))
             outfile_writer.writerow([total_elapsed_time, ax, ay, az, temp])
 
             while 1:
-                sample_elapsed_time = (datetime.now() - current_time).total_seconds()
+                sample_elapsed_time = (datetime.now() - sample_time)
                 if sample_elapsed_time >= frame_length:
                     break
 
